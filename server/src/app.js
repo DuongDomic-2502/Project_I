615c9ac5d9a41e3547c309ef52ce20b702305f95
DuongDomic-2502 <duongnguyen2502vn@gmail.com>
2026-06-18 10:03:17 +0700

    mongoDB connected

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,17 @@
 ﻿import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import helmet from "helmet";
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  res.send("API is running (ESM)...");
+  res.send("API is running...");
 });
 
 export default app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,17 @@
-﻿import dotenv from "dotenv";
-import app from "./app.js";
+﻿import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const app = express();
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});  
 });
